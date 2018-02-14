e20cd184e8ee74640d5b712edd1c1626375b4e7f
Kevin Gerken <kevin@kevingerken.com>
2018-02-13 18:30:58 -0600

    refactor demos

--- rgb/rgb.js
+++ rgb/rgb.js
@@ -14,17 +14,17 @@ const h = document.querySelector(`#hex`);
 const diff = document.querySelector(`#diff`);
 
 ng.addEventListener(`click`, newGame);
+
 easy.addEventListener(`click`, () => {
-    difficulty(3);
-    diff.value = 3;
+  difficulty(3);
 });
+
 hard.addEventListener(`click`, () => {
     difficulty(6);
-    diff.value = 6;
 });
+
 insanity.addEventListener(`click`, () => {
     difficulty(99);
-    diff.value = 99;
 });
 
 diff.addEventListener(`input`, () => {
@@ -32,83 +32,84 @@ diff.addEventListener(`input`, () => {
 });
 
 r.addEventListener(`click`, function () {
-    r.classList.add(`active`);
-    h.classList.remove(`active`);
-    colorType = `rgb`;
-    newGame();
+  r.classList.add(`active`);
+  h.classList.remove(`active`);
+  colorType = `rgb`;
+  newGame();
 })
 
 h.addEventListener(`click`, function () {
-    h.classList.add(`active`);
-    r.classList.remove(`active`);
-    colorType = `hex`;
-    newGame();
+  h.classList.add(`active`);
+  r.classList.remove(`active`);
+  colorType = `hex`;
+  newGame();
 })
 
 function difficulty(num) {
-    colorArray = colorArray.slice(0, 0);
-    section.innerHTML = ``;
-    for (let i = 0; i < num; i++) {
-        colorArray.push(``);
-        section.innerHTML += `<div class="color"></div>`;
-    }
-    let divs = document.querySelectorAll(`.color`);
-    for (let i = 0; i < divs.length; i++) {
-        colorArray[i] = divs[i];
-    }
-    newGame();
-}
-
-function randomColor() {
-    c = function () {
-        return Math.floor(Math.random() * 256);
-    }
-    return `rgb(${c()}, ${c()}, ${c()})`;
+  colorArray = colorArray.slice(0, 0);
+  section.innerHTML = ``;
+  for (let i = 0; i < num; i++) {
+    colorArray.push(``);
+    section.innerHTML += `<div class="color"></div>`;
+  }
+  let divs = document.querySelectorAll(`.color`);
+  for (let i = 0; i < divs.length; i++) {
+    colorArray[i] = divs[i];
+  }
+  diff.value = num;
+  newGame();
 }
 
 function newGame() {
-    win.textContent = ``;
-    header.style.backgroundColor = `#535993`;
-    colorArray.forEach(function(color) {
-        color.style.backgroundColor = randomColor();
-        color.style.opacity = 1;
-    }); 
-    winningColor = colorArray[Math.floor(Math.random() * colorArray.length)].style.backgroundColor;
-    if(colorType === `rgb`) {
-        find.innerHTML = winningColor;
-    } else {
-        find.innerHTML = rgbToHex(winningColor);
-    }
-    colorArray.forEach(function(color) {
-        divs = document.querySelectorAll(`.color`);
-        color.addEventListener(`click`, function(){
-            if (color.style.backgroundColor === winningColor) {
-                header.style.backgroundColor = winningColor;  
-                divs.forEach(function(color){
-                    color.style.backgroundColor = winningColor;
-                    color.style.opacity = 1;
-                })
-                win.textContent = `Winner!`;
-            } else {
-                color.style.opacity = 0;
-                win.textContent = `Try Again`;
-            }
-        });
-    }); 
+  win.textContent = ``;
+  header.style.backgroundColor = `#535993`;
+  colorArray.forEach(function(color) {
+    color.style.backgroundColor = randomColor();
+    color.style.opacity = 1;
+  }); 
+  winningColor = colorArray[Math.floor(Math.random() * colorArray.length)].style.backgroundColor;
+  if(colorType === `rgb`) {
+    find.innerHTML = winningColor;
+  } else {
+    find.innerHTML = rgbToHex(winningColor);
+  }
+  colorArray.forEach(function(color) {
+    divs = document.querySelectorAll(`.color`);
+    color.addEventListener(`click`, function(){
+      if (color.style.backgroundColor === winningColor) {
+        header.style.backgroundColor = winningColor;  
+        divs.forEach(function(color){
+          color.style.backgroundColor = winningColor;
+          color.style.opacity = 1;
+        })
+        win.textContent = `Winner!`;
+      } else {
+        color.style.opacity = 0;
+        win.textContent = `Try Again`;
+      }
+    });
+  }); 
+}
+
+function randomColor() {
+  c = function () {
+      return Math.floor(Math.random() * 256);
+  }
+  return `rgb(${c()}, ${c()}, ${c()})`;
 }
 
 function rgbToHex(rgb) {
-    rgbArr = rgb.slice(4, -1).split(`, `);
-    let hex = `#`;
-    rgbArr.map(o => {
-        testHex = Number(o).toString(16);
-        if(testHex.length < 2){
-            hex = hex + `0` + testHex;
-        } else {
-            hex += testHex;
-        }
-    });
-    return (hex);
+  rgbArr = rgb.slice(4, -1).split(`, `);
+  let hex = `#`;
+  rgbArr.map(o => {
+    testHex = Number(o).toString(16);
+    if(testHex.length < 2){
+      hex = hex + `0` + testHex;
+    } else {
+      hex += testHex;
+    }
+  });
+  return (hex);
 }
 
 difficulty(6);
--- theater/script.js
+++ theater/script.js
@@ -1,24 +1,27 @@
-var navVis = false;
-var nav = document.getElementById("nav");
-var burger = document.getElementById("burger");
-burger.addEventListener('click', navToggle);
-var body = document.getElementById("body");
+const nav = document.getElementById(`nav`);
+const burger = document.getElementById(`burger`);
+const body = document.getElementById(`body`);
+let navVis = false;
+
+burger.addEventListener(`click`, navToggle);
 
 function navToggle() {
-    if (navVis === true) {
-        fadeOut();
-    } else {
-        navVis = true;
-        nav.style.opacity = 1;
-        nav.style.visibility = "visible";
-        nav.addEventListener('click', fadeOut);
-        }
+  if (navVis === true) {
+    fadeOut();
+  } else {
+    navVis = true;
+    burger.value = `X`;
+    nav.style.opacity = 1;
+    nav.style.visibility = `visible`;
+    nav.addEventListener('click', fadeOut);
+  }
 }
      
 function fadeOut() {
-        navVis = false;
-        nav.style.opacity = 0;
-        setTimeout(function () {nav.style.visibility = "hidden";}, 1000) ;
+  navVis = false;
+  burger.value = `☰`;
+  nav.style.opacity = 0;
+  setTimeout(function () {nav.style.visibility = "hidden";}, 1000) ;
 }
 
 
