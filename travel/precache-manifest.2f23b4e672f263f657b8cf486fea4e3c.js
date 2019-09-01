fbcea5c17e5ff1845eb87515e4f688714d1e3447
Kevin Gerken <kevin@kevingerken.com>
2019-09-01 10:00:36 -0500

    change icons from cdn to svg, restyle header, add react icon

---
+++ travel/precache-manifest.2f23b4e672f263f657b8cf486fea4e3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b17a672108d23b0e89f0639cd75a83b",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "0cf95fa7dd22d13c760a",
+    "url": "/travel/static/css/main.25fd6816.chunk.css"
+  },
+  {
+    "revision": "ca3cc95f234d61c34cc7",
+    "url": "/travel/static/js/2.933a4120.chunk.js"
+  },
+  {
+    "revision": "0cf95fa7dd22d13c760a",
+    "url": "/travel/static/js/main.de8023d8.chunk.js"
+  },
+  {
+    "revision": "863f8cee14e2ca72744b",
+    "url": "/travel/static/js/runtime~main.11399f7e.js"
+  },
+  {
+    "revision": "e6b21723a05098f93479ff711f4b7bc2",
+    "url": "/travel/static/media/aboutme.e6b21723.jpg"
+  },
+  {
+    "revision": "9f656dfcd56b34fecd989fba9925d9f2",
+    "url": "/travel/static/media/gear.9f656dfc.jpg"
+  },
+  {
+    "revision": "a6536b97418ea1415118b0d13a8a399f",
+    "url": "/travel/static/media/judy.a6536b97.jpg"
+  },
+  {
+    "revision": "0af01d571e40e00e777a295f7bb9441f",
+    "url": "/travel/static/media/palm.0af01d57.png"
+  }
+]);
